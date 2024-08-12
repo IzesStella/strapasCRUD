28a81eeb6ef3defc6d740b3e4fa8d41ae3cacda2
IzesStella <isbb@discente.ifpe.edu.br>
2024-08-11 22:37:26 -0300

    add banco de dados

---
+++ bd.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `sistema`.`filmes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `diretor` VARCHAR(45) NOT NULL,
+  `duracao` VARCHAR(45) NOT NULL,
+  `protagonista` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
